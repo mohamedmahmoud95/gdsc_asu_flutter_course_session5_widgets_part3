89021827442cda1f851ab3584ebe84bada61955e
mohamreek <46673407+mohamedmahmoud95@users.noreply.github.com>
2023-04-26 08:47:02 +0200

    added list tile and listview.builder examples

--- lib/main.dart
+++ lib/main.dart
@@ -281,7 +281,7 @@ supportedDevices
 
 
 //-------------------------------------------------------------////-------------------------------------------------------------//
-//--------------------- GestureDetector widget ------------//
+//--------------------- List tile------------//
 //-------------------------------------------------------------////-------------------------------------------------------------//
 
 
@@ -289,7 +289,272 @@ supportedDevices
 
 
 //-------------------------------------------------------------//
-//
+//List tile
+/*
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+      //  backgroundColor: Colors.blue,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      child: Text('1', style: TextStyle(color: Colors.black),),
+                    ),
+                    title: Text("Item 1", style: TextStyle(color: Colors.black),),
+                    subtitle: Text("subtitle 1", style: TextStyle(color: Colors.black),),
+                    trailing: Icon(Icons.delete),
+                    tileColor: Colors.white,
+                    iconColor: Colors.black,
+                    textColor: Colors.black,
+                  ),
+                ),
+              ),
+
+
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Card(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.amber,
+                      child: Text('2', style: TextStyle(color: Colors.black),),
+                    ),
+                    title: Text("Item 2", style: TextStyle(color: Colors.black),),
+                    subtitle: Text("subtitle 2", style: TextStyle(color: Colors.black),),
+                    trailing: Icon(Icons.delete),
+                    tileColor: Colors.white,
+                    iconColor: Colors.black,
+                    textColor: Colors.black,
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+
+      ),
+    );
+  }
+}
+*/
+//-------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------//
+//List of List tiles
+/*
+class Contact{
+  String name;
+  String? phoneNumber;
+  String? imageURL;
+  Contact({required this.name,this.phoneNumber, this.imageURL});
+}
+
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  List<Contact> contacts = [
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png")
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Contacts"),
+        ),
+
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: contacts.map((contact) => Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: CircleAvatar(
+                          radius: 27,
+                          backgroundColor: Colors.white,
+                          child: Image.asset("${contact.imageURL}", ),
+                        ),
+                      ),
+                      title: Text(contact.name, style: const TextStyle(color: Colors.black),),
+                      subtitle: Text("${contact.phoneNumber}", style: const TextStyle(color: Colors.black),),
+                      trailing: const Icon(Icons.delete),
+                      tileColor: Colors.white,
+                      iconColor: Colors.black,
+                      textColor: Colors.black,
+                    ),
+                  ),
+              ),
+              ).toList(),
+
+            ),
+          ),
+        ),
+floatingActionButton: FloatingActionButton(onPressed: () {  },child: const Icon(Icons.add),),
+      ),
+    );
+  }
+}
+
+*/
+//-------------------------------------------------------------//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------------------------------------------------------//
+//ListView.builder:
+//List of list tiles
+/*
+
+class Contact{
+  String name;
+  String? phoneNumber;
+  String? imageURL;
+  Contact({required this.name,this.phoneNumber, this.imageURL});
+}
+
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  List<Contact> contacts = [
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Contacts"),
+        ),
+
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                Container(
+                height: 2000, //you must contain the listview.builder with a defined-size container
+                  //otherwise, you'll get RenderBox error (failed to assert: has size)
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+
+                  itemCount: contacts.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                    child: ListTile(
+                    leading: CircleAvatar(
+                    radius: 30,
+                    child: CircleAvatar(
+                    radius: 27,
+                    backgroundColor: Colors.white,
+                    child: Image.asset("${contacts[index].imageURL}", ),
+                    ),
+                    ),
+                    title: Text(contacts[index].name, style: const TextStyle(color: Colors.black),),
+                    subtitle: Text("${contacts[index].phoneNumber}", style: const TextStyle(color: Colors.black),),
+                    trailing: const Icon(Icons.delete),
+                    tileColor: Colors.white,
+                    iconColor: Colors.black,
+                    textColor: Colors.black,
+                    ),
+                    ),
+                    );
+
+                  }
+                ),
+              ),
+            ],
+            ),
+          ),
+        ),
+floatingActionButton: FloatingActionButton(onPressed: () {  },child: const Icon(Icons.add),),
+      ),
+    );
+  }
+}
+*/
 
 
 //-------------------------------------------------------------//
@@ -303,30 +568,112 @@ supportedDevices
 
 
 
-//-------------------------------------------------------------//
-//
 
 
 //-------------------------------------------------------------//
+//adding/deleting items from a list at runtime
 
+class Contact{
+  String name;
+  String? phoneNumber;
+  String? imageURL;
+  Contact({required this.name,this.phoneNumber, this.imageURL});
+}
 
+void main() => runApp(MyApp());
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<MyApp> {
 
+  List<Contact> contacts = [
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
+    Contact(name: "Mohamed Mahmoud", phoneNumber: '01001412578', imageURL: "assets/dash.png"),
+    Contact(name: "Ali Abdulrahman", phoneNumber: '01234567891', imageURL: "assets/dash2.png"),
+    Contact(name: "Mariam Alaa", phoneNumber: '01111011011', imageURL: "assets/dash3.png"),
 
+  ];
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Contacts"),
+        ),
 
+        body: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+                Container(
+                height: 700, //you must contain the listview.builder with a defined-size container
+                  //otherwise, you'll get RenderBox error (failed to assert: has size)
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
 
+                  itemCount: contacts.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                    child: ListTile(
+                    leading: CircleAvatar(
+                    radius: 33,
+                    child: CircleAvatar(
+                    radius: 27,
+                    backgroundColor: Colors.white,
+                    /////////////////////////////////////
+                      //to avoid run-time errors if the imageURL was null, add a default image
+                    foregroundImage: AssetImage("${contacts[index].imageURL!=null?contacts[index].imageURL:("assets/flappy_dash.gif")}"),
+                    ),
+                    ),
+                    title: Text(contacts[index].name, style: const TextStyle(color: Colors.black),),
+                    subtitle: Text("${contacts[index].phoneNumber}", style: const TextStyle(color: Colors.black),),
+                    trailing: IconButton(icon: Icon(Icons.delete), onPressed: () {
+                      setState(() {
+                        contacts.remove(contacts[index]);
+                      });
 
+                    },),
+                    tileColor: Colors.white,
+                    iconColor: Colors.black,
+                    textColor: Colors.black,
+                    ),
+                    ),
+                    );
+
+                  }
+                ),
+              ),
+            ],
+            ),
+          ),
+        ),
+floatingActionButton: FloatingActionButton(onPressed: () { 
+  setState(() {
+   
+      contacts.add(Contact(name: "New contact", phoneNumber: "123", ));
+  
+  });
+},child: const Icon(Icons.add),),
+      ),
+    );
+  }
+}
 
 //-------------------------------------------------------------//
-//
-
-
-//-------------------------------------------------------------//
-
-
-
 
 
 
